79e65578bf9a19600302191f6f4fa5a74090c12f
Guilherme Rosa <guilherme.rosa.santos@outlook.com>
2021-03-22 23:03:40 -0300

    atualiazando arquivos de configuracoes server

--- server.go
+++ server.go
@@ -1,12 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8000", nil)
 }
 
-func Hello(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("<h1>Hello Full Cycle!!!</h1>"))
+func Hello(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
 }
